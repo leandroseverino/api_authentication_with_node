fa8377934869f6900eb5b7a1bbfd2a315c25ed1c
unknown <Leandro Severino@POAW10N09344.resource.com.br>
2018-12-30 19:44:35 -0200

    Add Server Side Validation to routes entry point

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,6 @@
 module.exports = {
     signUp: async (req, res, next) => {
+        console.log('req.value.body', req.value.body);
         console.log('UsersController.signUp() called !');
     },
     signIn: async (req, res, next) => {
