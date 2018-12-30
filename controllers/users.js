a25f034b2691c673509fe4aa2ecbeedef1c85567
unknown <Leandro Severino@POAW10N09344.resource.com.br>
2018-12-30 20:41:07 -0200

    Add mongo/mongoose support

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,30 @@
+const User = require('../models/user');
+
 module.exports = {
     signUp: async (req, res, next) => {
-        console.log('req.value.body', req.value.body);
-        console.log('UsersController.signUp() called !');
+        const { email, password } = req.value.body;
+
+        // Check if there is a user with the same email
+        const foundedUser = await User.findOne({ email });
+
+        if (foundedUser) {
+            return res.status(409).json({ 'error': 'Email is already in use !.'});
+        }
+
+        // Save a new User
+        const newUser = new User({
+            email,
+            password
+        });
+
+        await newUser.save();
+
+        // Respond with the token
+        res.json({ user: 'Created !'});
+
     },
     signIn: async (req, res, next) => {
+        // Generate token
         console.log('UsersController.signIn() called !');
     },
     secret: async (req, res, next) => {
